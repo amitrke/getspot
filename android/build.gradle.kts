ecdef56d5f5394df87b38440894915bdd8bd17e0
Amit Kumar <amitrke@gmail.com>
2025-12-29 21:14:44 -0700

    Revert "chore: Refactor Gradle configuration and update dependencies for improved build management"
    This reverts commit 6690316b1f256ca4c7d37bbf0eb329349cc3ba4d.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,9 @@
-// Repositories are configured in settings.gradle.kts via dependencyResolutionManagement
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -16,18 +16,9 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
+    id("com.android.application") version "8.7.3" apply false
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version("4.3.15") apply false
     // END: FlutterFire Configuration
